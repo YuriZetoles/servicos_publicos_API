de856a2fe3f56929c76d30a97dcd068aeba5dbd0
Yuri <yurizetoles0123@gmail.com>
2025-09-09 21:36:30 -0400

    refactor: reorganizar importações e adicionar middleware de segurança

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-// import routes from "./routes/index.js";
-// import cors from "cors";
-// import helmet from "helmet";
-// import compression from "compression";
+// src/app.js
+
+import cors from "cors";
+import helmet from "helmet";
 import errorHandler from './utils/helpers/errorHandler.js';
 // import logger from './utils/logger.js';
-// import fileUpload from 'express-fileupload';
 import DbConnect from './config/dbConnect.js';
 import routes from './routes/index.js';
 import CommonResponse from './utils/helpers/CommonResponse.js';
 import express from "express";
 import expressFileUpload from 'express-fileupload';
+import compression from 'compression';
 
 const app = express();
 
@@ -17,6 +17,11 @@ await DbConnect.conectar();
 
 app.use(express.json()); // importante para ler JSON
 app.use(expressFileUpload());
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+
 
 routes(app);
 
@@ -27,7 +32,9 @@ app.use((req, res, next) => {
         404,
         'resourceNotFound',
         null,
-        [{ message: 'Rota não encontrada.' }]
+        [{
+            message: 'Rota não encontrada.'
+        }]
     );
 });
 
